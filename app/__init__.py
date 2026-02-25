ea0d649d7ae562ce6f816c1e61ca1f2736f7746b
Ewan Ferguson <esfergus@byu.edu>
2026-02-24 23:55:09 -0700

    Added HTML structure, CSS styling, and began dashboard page

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+
 def create_app(test_config=None):
     app = Flask(__name__,instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev'
+        SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path,'flaskr.sqlite'),
         )
     os.makedirs(app.instance_path, exist_ok=True)
     
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-        
+    @app.route('/')
+    def home_page():
+        return render_template('index.html')
+
+    @app.route('/dashboard')
+    def dashboard():
+        return render_template('dashboard.html')
+    
     return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
